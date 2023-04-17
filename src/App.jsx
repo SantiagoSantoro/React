3e0a73df6b79a5f9d44e36adcca2f3f9e7a2f877
Santiago Santoro <santiagosantoro10@gmail.com>
2023-04-17 20:44:16 -0300

    primer-pre-entrega

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,16 @@
 import "./App.css";
 import { ItemListContainer } from "./components/ItemListContainer/ItemListContainer";
 import { NavBar } from "./components/NavBar/NavBar";
+import rodneymullen from "./components/CartWidget/assets/rodneymullen.jpg";
+
 
 function App() {
-  return (<><NavBar /><ItemListContainer greeting="Bienvenidos a X-treme Clothes" /></>);
-}
+	return (
+	  <div className="App">
+		<NavBar />
+		<ItemListContainer greeting="Bienvenidos a X-treme Clothes" />
+	  </div>
+	);
+  }
 
 export default App;
--- src/components/ItemListContainer/ItemListContainer.jsx
+++ src/components/ItemListContainer/ItemListContainer.jsx
@@ -1,5 +1,5 @@
 import Container from "react-bootstrap/Container";
 
 export const ItemListContainer = ({greeting}) => {
-   return (<Container className="mt-4"><h1>{greeting}</h1></Container>);
+   return (<Container className="mt-4"><h1 className="title">{greeting}</h1></Container>);
 }
