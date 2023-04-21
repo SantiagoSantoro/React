c7ae0b5a09918d44b498c18e590a98391f5d88b6
Santiago Santoro <santiagosantoro10@gmail.com>
2023-04-21 15:00:40 -0300

    listo por hoy

--- src/components/ItemList/ItemList.jsx
+++ src/components/ItemList/ItemList.jsx
@@ -3,7 +3,7 @@ import { Item } from "../Item/Item";
 
 export const ItemList = ({ list }) => (
   <>
-    {!list.length && "Loading"}
+    {!list.length && <div className="loading">Loading...</div>}
     <Row>
       {list.slice(0, 4).map((item) => (
         <Col key={item.id}>
@@ -20,5 +20,6 @@ export const ItemList = ({ list }) => (
     </Row>
   </>
 );
+
 // // La función slice se utiliza para separar los elementos de la lista en dos grupos de cuatro elementos cada uno, para crear las dos filas. 
 // Luego, para cada elemento de la lista, estamos creando un componente Col y dentro de él estamos renderizando el componente Item con el objeto de la lista correspondiente.
