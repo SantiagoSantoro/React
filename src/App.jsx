464ed8204316124ff48612be9e3e31a6f6c2c40d
Santiago Santoro <santiagosantoro10@gmail.com>
2023-05-09 20:59:07 -0300

    navbarbao

--- src/App.jsx
+++ src/App.jsx
@@ -2,21 +2,23 @@ import "./App.css";
 import { ItemDetailContainer } from "./components/ItemDetailContainer/ItemDetailContainer";
 import { ItemListContainer } from "./components/ItemListContainer/ItemListContainer";
 import { NavBar } from "./components/NavBar/NavBar";
-import { BrowserRouter, Routes, Route} from "react-router-dom";
-
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { CartProvider } from "./components/Context/CartContext";
 
 function App() {
 	return (
-	  <BrowserRouter>
-		<NavBar />
-		<Routes>
-		<Route path="/" element={<ItemListContainer greeting="Bienvenidos a X-Treme Shop" />} />
-		  <Route path="/item/:id" element={<ItemDetailContainer />} />
-		  <Route path="/category/:id" element={<ItemListContainer />} />
-		</Routes>
-	  </BrowserRouter>
+		<CartProvider>
+			<BrowserRouter>
+				<NavBar />
+				<Routes>
+					<Route path="/" element={<ItemListContainer greeting="Bienvenidos a X-Treme Shop" />} />
+					<Route path="/item/:id" element={<ItemDetailContainer />} />
+					<Route path="/category/:id" element={<ItemListContainer />} />
+				</Routes>
+			</BrowserRouter>
+		</CartProvider>
 	);
-  }
+}
 
 export default App;
 
